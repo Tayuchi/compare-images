f293f013993359ee907ca62c581b0e734b84948b
dokimiki <57443812+dokimiki@users.noreply.github.com>
2024-05-11 10:04:46 +0900

    Update pictures.py

--- pictures.py
+++ pictures.py
@@ -106,10 +106,11 @@ def compare_images():
 
     image_url2 = data['image_url2']
     try:
-        base64_encoded_data = image_url2.split(',')[1]
         logger.info("Getting image_url2 data...")
-        image_data2 = base64.b64decode(base64_encoded_data)
+        base64_encoded_data = image_url2.split(',')[1]
         logger.info("Decoding image_url2 data...")
+        image_data2 = base64.b64decode(base64_encoded_data)
+        logger.info("Finished decoding and process!")
     except Exception as e:
         logger.error(f"Failed to decode or process image_url2: {str(e)}")
         return jsonify({"error": f"Failed to decode or process image_url2: {str(e)}"}), 400
